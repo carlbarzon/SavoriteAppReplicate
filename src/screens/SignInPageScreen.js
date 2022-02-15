29231da675a10103a3b2ba313e2b1105b84623c3
Carl Joshua <qcjplalongbilang@tip.edu.ph>
2022-02-15 09:43:32 +0800

    SignIn and SignUp Page Added

---
+++ src/screens/SignInPageScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const SignInPageScreen = () => {
+  //arrow function and function() is just the same
+
+  return (
+    <View>
+      <Text>Savorite</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignInPageScreen;
